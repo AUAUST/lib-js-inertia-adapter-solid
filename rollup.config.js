528a62f82dddd1a33217166bb2384fd5df21f307
apademide <contact@apademide.ch>
2024-04-15 16:15:51 +0200

    files: only `.tsx` for files using JSX + update entry points

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,33 @@ import withSolid from "rollup-preset-solid";
 
 export default withSolid([
   {
-    input: "src/index.tsx",
-    targets: ["esm", "cjs"],
+    input: "src/index.ts",
+    output: [
+      {
+        format: "esm",
+        file: "dist/index/index.mjs",
+        sourcemap: true,
+      },
+      {
+        format: "cjs",
+        file: "dist/index/index.cjs",
+        sourcemap: true,
+      },
+    ],
   },
   {
-    input: "src/server.tsx",
-    targets: ["esm", "cjs"],
+    input: "src/server.ts",
+    output: [
+      {
+        format: "esm",
+        file: "dist/server/index.mjs",
+        sourcemap: true,
+      },
+      {
+        format: "cjs",
+        file: "dist/server/index.cjs",
+        sourcemap: true,
+      },
+    ],
   },
 ]);
