9a13a1a92044c532719ef19c5a0f78d73bf8507d
apademide <contact@apademide.ch>
2024-04-15 14:33:42 +0200

    imp: Primitive Kit > Lodash

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,3 @@ export { default as createInertiaApp } from "./createInertiaApp";
 export { default as useForm } from "./useForm";
 export { default as usePage } from "./usePage";
 export { default as useRemember } from "./useRemember";
-
-export function test() {
-  return (
-    <div>
-      <h1>Test</h1>
-      Hello World
-    </div>
-  );
-}
