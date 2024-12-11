52e960e48b25d2df47b1af75eca37e14ee4d492d
apademide <contact@apademide.ch>
2024-12-11 17:15:11 +0100

    meta: build with tsup and tsup-preset-solid

---
+++ tsup.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from "tsup";
+import {
+  type PresetOptions,
+  generatePackageExports,
+  generateTsupOptions,
+  parsePresetOptions,
+  writePackageJson,
+} from "tsup-preset-solid";
+
+const preset_options: PresetOptions = {
+  entries: [
+    {
+      // entries with '.tsx' extension will have `solid` export condition generated
+      entry: "src/index.tsx",
+      dev_entry: true,
+    },
+    {
+      entry: "src/server.ts",
+      server_entry: true,
+    },
+  ],
+  cjs: true,
+};
+
+const CI =
+  process.env["CI"] === "true" ||
+  process.env["GITHUB_ACTIONS"] === "true" ||
+  process.env["CI"] === '"1"' ||
+  process.env["GITHUB_ACTIONS"] === '"1"';
+
+export default defineConfig((config) => {
+  const watching = !!config.watch;
+
+  const parsed_options = parsePresetOptions(preset_options, watching);
+
+  if (!watching && !CI) {
+    const package_fields = generatePackageExports(parsed_options);
+
+    console.log(
+      `package.json: \n\n${JSON.stringify(package_fields, null, 2)}\n\n`
+    );
+
+    // will update ./package.json with the correct export fields
+    writePackageJson(package_fields);
+  }
+
+  return generateTsupOptions(parsed_options);
+});
