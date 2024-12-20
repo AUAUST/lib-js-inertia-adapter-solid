60ec3a487e6a2609c0d448797d6105f49e824078
apademide <contact@apademide.ch>
2024-12-20 17:35:12 +0100

    feat: `hydrateOrRender`

--- src/extras/index.ts
+++ src/extras/index.ts
@@ -1,12 +1,13 @@
+import { P } from "@auaust/primitive-kit";
 import { router } from "@inertiajs/core";
-import { isDev, isServer } from "solid-js/web";
+import { hydrate, isDev, isServer, render } from "solid-js/web";
 
 /**
  * Can be called once to automatically expose the Inertia page and props state
  * under to `$page` and `$props` window properties. A boolean can be passed to
  * conditionally enable the feature. Will only be enabled in the browser.
  */
-export function exposeInertiaState(enabled: boolean = true) {
+export const exposeInertiaState = (enabled: boolean = true) => {
   if (!enabled || isServer || typeof window === "undefined") return;
 
   router.on("navigate", (e) => {
@@ -17,4 +18,37 @@ export function exposeInertiaState(enabled: boolean = true) {
 
     isDev && console.info("Navigation event, `$page` and `$props` updated.");
   });
-}
+};
+
+/**
+ * Returns a boolean indicating if the document has been server-rendered.
+ * Always returns `false` in the server.
+ */
+export const isServerRendered = () => {
+  return (
+    !isServer &&
+    typeof window !== "undefined" &&
+    // @ts-ignore - This key is set by the hydration script the backend adds when server-rendering.
+    P.isSet(window._$HY)
+  );
+};
+
+/**
+ * Returns either `hydrate` or `render` depending on if the document has been
+ * server-rendered. Always returns `render` in the server.
+ *
+ * This is useful to handle the possibility of the backend failing to render.
+ */
+export const hydrateOrRender: typeof hydrate = (...args) => {
+  const method = isServerRendered() ? hydrate : render;
+
+  if (isDev) {
+    if (method === render) {
+      console.warn("The application is being client-side rendered.");
+    } else {
+      console.info("The application is being hydrated.");
+    }
+  }
+
+  return method(...args);
+};
